0042bda40bfab905b61e986a0da032b69a538faa
T510 <oleg.chudnovskiy@gmail.com>
2019-07-11 08:25:51 +0300

    Add test for Randomizer, and empty ConnectionInfoServiceTest class

--- test/com/company/util/RandomazerTest.java
+++ test/com/company/util/RandomazerTest.java
@@ -8,9 +8,15 @@ class RandomazerTest {
 
     @Test
     void testGetRandomNumber() {
+        Integer testNumber = Randomazer.getRandomNumber( 50, 100);
+        assertNotNull(testNumber);
+        assertTrue(testNumber >= 50 & testNumber <= 100);
     }
 
     @Test
     void testGetRandomString() {
+        String  testString = Randomazer.getRandomString(20);
+        assertNotNull(testString);
+        assertEquals(20, testString.length());
     }
 }
